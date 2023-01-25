a4899d9f68599e8208e1195de7b79af438e89a25
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2023-01-25 19:45:58 +0500

    Update tool-translate.js

--- plugins/tool-translate.js
+++ plugins/tool-translate.js
@@ -1,8 +1,8 @@
 import translate from '@vitalets/google-translate-api'
 import fetch from 'node-fetch'
 let handler = async (m, { args, usedPrefix, command }) => {
-let msg = `*[🙈 𝐈𝐍𝐅𝐎 🙈] 𝚄𝚂𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command} (idioma) (texto)*\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} es Hello*\n\n*𝙲𝙾𝙽𝙾𝙲𝙴 𝙻𝙾𝚂 𝙸𝙳𝙸𝙾𝙼𝙰𝚂 𝙰𝙳𝙼𝙸𝚃𝙸𝙳𝙾𝚂 𝙴𝙽:*\n*- https://cloud.google.com/translate/docs/languages*`
-if (!args || !args[0]) return m.reply(msg)  
+let msg = `*[🙈 𝐈𝐍𝐅𝐎 🙈] 𝑪𝒐𝒓𝒓𝒆𝒄𝒕 𝑼𝒔𝒆 𝑶𝒇 𝑻𝒉𝒆 𝑪𝒐𝒎𝒎𝒂𝒏𝒅 ${usedPrefix + command} (idioma) (texto)*\n*𝑬𝒙𝒂𝒎𝒑𝒍𝒆:*\n*${usedPrefix + command}  Hello*\n\n*𝑲𝒏𝒐𝒘𝒔 𝑻𝒉𝒆 𝑳𝒂𝒏𝒈𝒖𝒂𝒈𝒆𝒔 𝑺𝒖𝒑𝒑𝒐𝒓𝒕𝒆𝒅 𝑰𝒏 :*\n*- https://cloud.google.com/translate/docs/languages*`
+if (!args || !args[0]) return m.reply(msg) 
 let lang = args[0]
 let text = args.slice(1).join(' ')
 const defaultLang = 'es'
@@ -20,7 +20,7 @@ let loll = await lol.json()
 let result2 = loll.result.translated
 await m.reply('*Traducción:* ' + result2)
 } catch { 
-await m.reply('*[🙈 𝐈𝐍𝐅𝐎 🙈] ERROR, VUELVA A INTENTARLO*')    
+await m.reply('*[🙈 𝐈𝐍𝐅𝐎 🙈] ERROR, TRY AGAIN☺️*')    
 }}}
-handler.command = /^(translate|traducir|trad)$/i
+handler.command = /^(translate|trt|trad)$/i
 export default handler
